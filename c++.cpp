12d139d194f6085b2196d94112474167d04b2a8a
manjot <jotm1458@gmail.com>
2026-06-24 19:41:56 +0530

    https://github.com/manjotmannkaur31/ass1

--- c++.cpp
+++ c++.cpp
@@ -1,10 +1,121 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 int main()
 {
-<<<<<<< Updated upstream
-    cout<<"hello ";
-=======
-    cout << "hello mann ";
->>>>>>> Stashed changes
+   int n;
+   cout<<"enter the number=";
+   cin>>n;
+for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+        cout<<i;}
+        cout<<endl;
 }
+}*/
+/*#include<iostream>
+using namespace std;
+int main (){
+    for (int i=1;i<=3;i++){
+        for(int j=1;j<=3;j++){
+        cout<<"*";
+    } cout<<endl;
+    }
+}*/
+/*#include<iostream>
+using namespace std;
+int main(){
+        for(int i =1;i<4;i++){{
+                cout<<i<<"*"<<i<<"="<<i*i;
+            }cout<<endl;
+        }
+}*/
+/*#include<iostream>
+using namespace std;;
+int main(){
+    int n;
+    cout<<"enter the number =";
+    cin>>n;
+    for(int i=1;i<=2;i++){
+        for(int j=1;j<=n;j++){
+    cout<< j ;}
+    cout<<endl;
+        }
+}*/
+/*#include<iostream>
+using namespace std;
+int main (){
+    string n;
+    cout<<"enter the number =";
+    cin>>n;
+    for(int i=0;i<=3;i++){
+        for(int j=0;j<=3;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+/*#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }cout<<endl;
+    }
+}
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the number =";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+    cout<<j;
+        }
+        cout<<endl;
+    }
+}
+#include<iostream>
+using namespace std;
+int main(){
+    
+    for(int i=1;i<=2;i++){
+        for(int j=1;j<=3;j++){
+            cout<<"*";
+        }cout<<endl;}cout<<endl;
+        for(int i=1;i<=3;i++){
+            cout<<"*";
+        
+    }
+}/*
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    cout<<"enter the number =";
+    cin>>n;
+    for( int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i;
+        }cout<<endl;
+    }
+}*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the number =";
+    cin>>n;
+    for(int i=3;i>=1;i--){
+for(int j=n;j>=1;j--){
+    cout<<i;
+}cout<<endl;
+    }
+
+}
+
+
+
+
+
